d874e672ff5682395f34d991c60a4af50ffea143
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2022-07-25 08:26:19 +0100

    added port and Mongo connections

--- server.js
+++ server.js
@@ -16,4 +16,8 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port = ${process.env.PORT}`)
+})
     
